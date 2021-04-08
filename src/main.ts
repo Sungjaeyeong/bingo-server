5923b6d399e56324fabfa7623eed1c1da293602f
성재영 <jaeyeong@MacBookPro.local>
2021-04-08 18:00:12 +0900

    Fix testdeploy

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ["https://localhost:3000", "http://ibingo.link.s3-website.ap-northeast-2.amazonaws.com", "https://ibingo.link"],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
